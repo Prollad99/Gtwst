3482b3e6bdb6765dfa6acd30008226d720e28da4
Prollad99 <prolladmail@gmail.com>
2024-06-07 12:32:10 +0530

    Update fetch-links.js

--- fetch-links.js
+++ fetch-links.js
@@ -2,15 +2,20 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+console.log("Starting script...");
+
 const url = 'https://techyhigher.com/gametwist-slots-free-coins/';
 
 axios.get(url).then(({ data }) => {
+  console.log("Fetched data...");
   const $ = cheerio.load(data);
   const links = [];
   $('a[href*="gametwist"]').each((index, element) => {
     links.push($(element).attr('href'));
   });
+  console.log("Links fetched:", links);
   fs.writeFileSync('links.json', JSON.stringify(links));
+  console.log("Links saved to links.json");
 }).catch(err => {
   console.error('Error fetching links:', err);
 });
